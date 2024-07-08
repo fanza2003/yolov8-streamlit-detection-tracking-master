5c024bb234086f163c48e4750c1222ce7f591e1d
fanza2003 <fanza2003@gmail.com>
2024-07-08 23:30:03 +0700

    ganti

--- init_db.py
+++ init_db.py
@@ -17,7 +17,8 @@ users = [
     ('admin', 'Admin', hash_password('your_admin_password')),
     ('rmiller', 'Rebecca Miller', hash_password('your_rebecca_password')),
     ('fanza', 'eldanendra', hash_password('123')),
-    ('Admin', 'Admin123', hash_password('123'))
+    ('admin', 'Admin', hash_password('123')),
+    ('Admin', 'Admin', hash_password('123'))
 ]
 
 c.executemany('INSERT OR REPLACE INTO users VALUES (?,?,?)', users)
